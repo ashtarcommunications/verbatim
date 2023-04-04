5626f1d42c84ab076732773d903dd18eb568fea2
Aaron Hardy <ashtarcommunications@gmail.com>
2023-04-04 11:38:27 -0600

    Slashes the wrong way

--- desktop/bin/install-dev.bat
+++ desktop/bin/install-dev.bat
@@ -1,2 +1,2 @@
-copy ../Debate.dotm %AppData%\Microsoft\Templates\Debate.dotm
-copy ../DebateStartup.dotm %AppData%\Microsoft\Word\STARTUP\DebateStartup.dotm
+copy ..\Debate.dotm %AppData%\Microsoft\Templates\Debate.dotm
+copy ..\DebateStartup.dotm %AppData%\Microsoft\Word\STARTUP\DebateStartup.dotm
